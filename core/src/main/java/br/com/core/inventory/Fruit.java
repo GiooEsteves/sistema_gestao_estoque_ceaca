e73aad5baa3d633ba0e496931547e0e0b83a9148
Giovanna Esteves <gio_esteves@hotmail.com>
2024-11-24 12:57:00 -0300

    aCommit

--- core/src/main/java/br/com/core/inventory/Fruit.java
+++ core/src/main/java/br/com/core/inventory/Fruit.java
@@ -2,7 +2,7 @@ package br.com.core.inventory;
 
 public class Fruit extends Product {
 
-    public Fruit(String name, int quantity, float unitPrice) {
-        super(name, quantity, unitPrice);
+    public Fruit(int id, String name, int quantity, float unitPrice) {
+        super(id, name, quantity, unitPrice);
     }
 }
